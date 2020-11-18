0b5cc8785e83064533869e58cc94b745b1d2d750
tungtt <tungthanh1497@gmail.com>
2020-11-18 09:15:34 +0700

    generic java

--- src/com/tungtt/Main.java
+++ src/com/tungtt/Main.java
@@ -7,29 +7,29 @@ import java.util.Random;
 public class Main {
 
     private static Random random;
-    private static ThreadManager threadManager;
 
     public static void main(String[] args) {
         random = new Random();
-
-
-        threadManager = new ThreadManager();
-
-        for (int i = 0; i < 10; i++) {
-            threadManager.execute(new SendRequestTask("SRT" + (i + 1), random.nextInt(50)));
-        }
-        for (int i = 0; i < 10; i++) {
-            threadManager.execute(new UploadTask("UT" + (i + 1), random.nextInt(50)));
-        }
-        for (int i = 0; i < 10; i++) {
-            threadManager.execute(new GetDataTask("GDT" + (i + 1), random.nextInt(50)));
-        }
-        for (int i = 0; i < 10; i++) {
-            threadManager.execute(new InsertDataLocalTask("IDLT" + (i + 1), random.nextInt(50)));
-        }
-        for (int i = 0; i < 10; i++) {
-            threadManager.execute(new DoActionTask("DAT" + (i + 1), random.nextInt(50)));
+        ThreadManager threadManager = new ThreadManager();
+        for (int i = 0; i < 50; i++) {
+            threadManager.execute(generateNewTask(i + 1));
         }
 
+    }
+
+    private static <T extends BaseWorker> T generateNewTask(int index) {
+        switch (random.nextInt(5)) {
+            case 1:
+                return (T) new UploadTask("UT" + index, random.nextInt(50));
+            case 2:
+                return (T) new GetDataTask("GDT" + index, random.nextInt(50));
+            case 3:
+                return (T) new InsertDataLocalTask("IDLT" + index, random.nextInt(50));
+            case 4:
+                return (T) new DoActionTask("DAT" + index, random.nextInt(50));
+            case 0:
+            default:
+                return (T) new SendRequestTask("SRT" + index, random.nextInt(50));
+        }
     }
 }
